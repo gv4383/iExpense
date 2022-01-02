d8a6b88d709e656b9586632bba3269f5c2ec0190
gv4383 <gregory.vargas18@gmail.com>
2022-01-02 01:07:33 -0600

    Split expenses into two separate lists

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -17,24 +17,55 @@ struct ContentView: View {
     @StateObject var expenses = Expenses()
     @State private var showingAddExpense = false
     
+    var personalExpenses: [ExpenseItem] {
+        expenses.items.filter { $0.type == "Personal" }
+    }
+    var businessExpenses: [ExpenseItem] {
+        expenses.items.filter { $0.type == "Business" }
+    }
+    
     var body: some View {
         NavigationView {
             List {
-                ForEach(expenses.items) { item in
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(item.name)
-                                .font(.headline)
-                            Text(item.type)
+                if personalExpenses.count > 0 {
+                    Section("Personal") {
+                        ForEach(personalExpenses) { item in
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text(item.name)
+                                        .font(.headline)
+                                    Text(item.type)
+                                }
+                                
+                                Spacer()
+                                
+                                Text(item.amount, format: .currency(code: item.currency))
+                                    .setStyleForAmount(item.amount)
+                            }
                         }
-                        
-                        Spacer()
-                        
-                        Text(item.amount, format: .currency(code: item.currency))
-                            .setStyleForAmount(item.amount)
+                        .onDelete(perform: removePersonalItems)
                     }
                 }
-                .onDelete(perform: removeItems)
+                
+                if businessExpenses.count > 0 {
+                    Section("Business") {
+                        ForEach(businessExpenses) { item in
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text(item.name)
+                                        .font(.headline)
+                                    Text(item.type)
+                                }
+                                
+                                Spacer()
+                                
+                                Text(item.amount, format: .currency(code: item.currency))
+                                    .setStyleForAmount(item.amount)
+                            }
+                        }
+                        .onDelete(perform: removeBusinessItems)
+                    }
+                }
             }
             .navigationTitle("iExpense")
             .toolbar {
@@ -50,8 +81,26 @@ struct ContentView: View {
         }
     }
     
-    func removeItems(at offsets: IndexSet) {
-        expenses.items.remove(atOffsets: offsets)
+    func removePersonalItems(at offsets: IndexSet) {
+        for offset in offsets {
+            let itemId = personalExpenses[offset].id
+            let itemIndex = expenses.items.firstIndex { item in
+                return item.id == itemId
+            }
+            
+            expenses.items.remove(at: itemIndex!)
+        }
+    }
+    
+    func removeBusinessItems(at offsets: IndexSet) {
+        for offset in offsets {
+            let itemId = businessExpenses[offset].id
+            let itemIndex = expenses.items.firstIndex { item in
+                return item.id == itemId
+            }
+            
+            expenses.items.remove(at: itemIndex!)
+        }
     }
 }
 
